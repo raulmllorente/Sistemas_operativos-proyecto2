dad82b29697482cc43e5579d7aa28d4d4c5c7ed1
pva-bit <pedro.ventura@live.u-tad.com>
2021-01-14 18:37:48 +0100

    Modificación y arreglo del comprobar comando, imprimir y desarollo de copiar y borrar

--- simul_ext.c
+++ simul_ext.c
@@ -7,12 +7,12 @@
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps);
 int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
-int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
+int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
               char *nombre);
 void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos);
-int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
+int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
               char *nombreantiguo, char *nombrenuevo);
-int Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
+int Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
              EXT_DATOS *memdatos, char *nombre);
 int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
            EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
@@ -33,7 +33,7 @@ int  BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombre
 		if (strcmp(nombre,directorio[x].dir_nfich)==0) {
 			p=x;
 		}
-	}
+	} 
 	return p;
 }
 
@@ -65,14 +65,18 @@ void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps) {
 void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos) {			// 20 ficheros 1- raiz
 	int x, y;
 
-	for (x = 1;x < MAX_FICHEROS && directorio[x].dir_inodo != 0xffff; x++) {		// hasta 0xffff para evitar basura
-		printf("%s \t", directorio[x].dir_nfich);
-		printf(" tamaño: %d \t", inodos->blq_inodos[directorio[x].dir_inodo].size_fichero);
-		printf(" inodo: %d \t", directorio[x].dir_inodo);
-		printf(" Bloques:");
-		for (y = 0; y < MAX_NUMS_BLOQUE_INODO && inodos->blq_inodos[directorio[x].dir_inodo].i_nbloque[y] != 0xffff; y++) {
-			printf(" %d", inodos->blq_inodos[directorio[x].dir_inodo].i_nbloque[y]);
-		}				printf("\n");
+	for (x = 1;x < MAX_FICHEROS ; x++) {		
+		if(directorio[x].dir_inodo != NULL_INODO){// hasta 0xffff para evitar basura
+			printf("%s \t", directorio[x].dir_nfich);
+			printf(" tamaño: %d \t", inodos->blq_inodos[directorio[x].dir_inodo].size_fichero);
+			printf(" inodo: %d \t", directorio[x].dir_inodo);
+			printf(" Bloques:");
+			for (y = 0; y < MAX_NUMS_BLOQUE_INODO; y++){
+				if(inodos->blq_inodos[directorio[x].dir_inodo].i_nbloque[y] != NULL_BLOQUE){
+					printf(" %d", inodos->blq_inodos[directorio[x].dir_inodo].i_nbloque[y]);
+				}	
+			}printf("\n");
+		}
 	}
 }
 
@@ -81,37 +85,35 @@ int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombrea
 	int p1=BuscaFich(directorio, inodos,nombreantiguo);
 	int p2=BuscaFich(directorio, inodos,nombrenuevo);
 
-	if(p1==0){
+	if(p1==0){		  
 		printf("ERROR: Fichero %s no encontrado\n", nombreantiguo);
 		return -1;
 	}
-
-	if(p2!=0){
+	
+	if(p2!=0){		 
 		printf("ERROR: El fichero  %s ya existe \n", nombrenuevo);
 		return -1;
 	}
-
+		
 	strcpy(directorio[p1].dir_nfich, nombrenuevo);
-	return 0;
+	return 0; 
 }
 
 
 int Imprimir(EXT_ENTRADA_DIR* directorio, EXT_BLQ_INODOS* inodos, EXT_DATOS* memdatos, char* nombre) {
-	int p1 = 0;
 	int x;
-
-	char texto[MAX_NUMS_BLOQUE_INODO];
-	p1 = BuscaFich(directorio, inodos, nombre);
-
-	if (p1 == 0) {
+	int aux = 0;
+	EXT_DATOS texto[MAX_NUMS_BLOQUE_INODO];
+	int p2= BuscaFich(directorio, inodos, nombre);
+	if(p2==0){
 		printf("ERROR: Fichero %s no encontrado\n", nombre);
 		return -1;
 	}
-
-	for (x = 0; x < MAX_NUMS_BLOQUE_INODO && inodos->blq_inodos[directorio[p1].dir_inodo].i_nbloque[x] != 0xffff; x++) {
-						// añadir los datos
+	for(x = 0; x < MAX_NUMS_BLOQUE_INODO && inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque !=0 ; x++){
+		if(inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[x] != NULL_BLOQUE){
+			strcpy(&texto[x], &memdatos[inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[x]- PRIM_BLOQUE_DATOS]);
+		}	
 	}
-
 	memcpy(&texto[x], "\0", 1);
 	puts(texto);
 	return 0;
@@ -120,54 +122,72 @@ int Imprimir(EXT_ENTRADA_DIR* directorio, EXT_BLQ_INODOS* inodos, EXT_DATOS* mem
 int Copiar(EXT_ENTRADA_DIR* directorio, EXT_BLQ_INODOS* inodos, EXT_BYTE_MAPS* ext_bytemaps, EXT_SIMPLE_SUPERBLOCK* ext_superblock, EXT_DATOS* memdatos, char* nombreorigen, char* nombredestino, FILE* fich) {
 	int p1 = 0;
 	int p2 = 0;
-	int x, j, w;
-
+	int x, j, w,t ;
+	int aux;
+	
 	p1 = BuscaFich(directorio, inodos, nombreorigen);
 	p2 = BuscaFich(directorio, inodos, nombredestino);
 
 	if (p1 == 0) {
-		printf("\n ERROR: Fichero %s no encontrado", nombreorigen);
+		printf("ERROR: Fichero %s no encontrado\n", nombreorigen);
 		return -1;
 	}
 	if (p2 != 0) {
 		printf("ERROR: El fichero  %s ya existe \n", nombredestino);
 		return -1;
 	}
-
-	for (x = 1; x < sizeof(directorio); x++) {
-		if (directorio[x].dir_nfich != '\0') {
-			strcpy(directorio[x].dir_nfich, nombredestino);			// copiamos nombre
-		}
-	}
-
-	for (w = directorio[p1].dir_inodo; w < MAX_INODOS; w++) {
-		if (ext_bytemaps->bmap_inodos[w] == 0) {
-			directorio[x].dir_inodo = w;
+	
+ 
+	aux=inodos->blq_inodos[directorio[x].dir_inodo].i_nbloque[0]; // cogemos el primer bloque ocupado
+	
+	
+	for (x = 1; x < sizeof(directorio); x++) {			// ver si es dsp del fichero anterior o no
+		if (directorio[x].dir_nfich == NULL) {															// cambiar
+			strcpy(directorio[x].dir_nfich, nombredestino);			// copiamos nombre en el array de 
 			inodos->blq_inodos[directorio[x].dir_inodo].size_fichero = inodos->blq_inodos[directorio[p1].dir_inodo].size_fichero;
+			for(j=p2 ; j< MAX_INODOS; j++){							// j = entrada indo fichero viejo 
+				if(ext_bytemaps->bmap_inodos[j]==0){
+					directorio[x].dir_inodo= j;			// guardamos siguiente inodo
+					ext_bytemaps->bmap_inodos[j]=1;
+				}
+			}
+			for(w=aux; w< MAX_BLOQUES_PARTICION; w++){				// empiza por el primer bloq ocupado 
+				if(ext_bytemaps->bmap_bloques[w] == 0){
+					ext_bytemaps->bmap_bloques[w]=1;
+					inodos->blq_inodos[directorio[x].dir_inodo].i_nbloque[w];		// añdir los datos a los bloques correspondientes
+				}
+			}
 		}
 	}
+	
+	
 	return 0;
 }
 
 
 int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock, char *nombre,  FILE *fich){
 	int p2=BuscaFich(directorio, inodos,nombre);
-	int x,j;
-	int w=0;
-
+	int x=0;
+	int aux1=0;
+	int aux2=0;
+	int j;
+	
 	if(p2==0){
-		printf("\n ERROR: Fichero %s no encontrado", nombre);
+		printf("ERROR: Fichero %s no encontrado \n", nombre);	
 		return -1;
 	}
-																						// MODIFICAR
-	ext_bytemaps->bmap_inodos[directorio[p2].dir_inodo]=0;
-	for(j=0; j<MAX_NUMS_BLOQUE_INODO && inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[x] != 0xffff ; j++){		// bytemaps de bloques 
-		w=inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[j];
-		ext_bytemaps->bmap_bloques[w]=0;
-		inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[j]=0xffff;
+	
+ 	ext_bytemaps->bmap_inodos[directorio[p2].dir_inodo]=0;
+	
+	for(j=0; j< MAX_NUMS_BLOQUE_INODO && inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque !=0; j++){		// bytemaps de bloques 
+		if(inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[x] != NULL_BLOQUE){
+			aux2=inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[j]; 
+		//	ext_bytemaps->bmap_bloques[aux2]=0;									// peta
+			inodos->blq_inodos[directorio[p2].dir_inodo].i_nbloque[j]=5;	
+		}
 	}
-	strcpy(&directorio[p2].dir_nfich,"");
-	directorio[p2].dir_inodo=0xffff;
+	strcpy(directorio[p2].dir_nfich,"");
+	directorio[p2].dir_inodo=5;								// peta
 	inodos->blq_inodos[directorio[p2].dir_inodo].size_fichero= 0;
 
 	return 0;
@@ -175,23 +195,23 @@ int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_BYTE_MAPS *e
 
 
 int ComprobarComando(char* strcomando, char* orden, char* argumento1, char* argumento2) { // strcomando todo lo que pasa al buffer
-    char* delim;
-    delim = strtok(strcomando, "\n");
+    char *delim;
 
-     if(strcmp(strcomando, " ")==0){
-         strcpy(argumento1, delim);
-        if(delim != NULL){
-            delim = strtok(NULL, " ");
-            strcpy(argumento2, delim);
+    delim = strtok(strcomando, "\n");				// eliminar /n fgets 
+    delim = strtok(strcomando, " ");
+    strcpy(orden, delim);						// copiar orden sin argumentos 
+	delim = strtok(NULL, " ");
+     
+    if(delim != NULL){						// 1 argumento 
+       strcpy(argumento1, delim);
+       delim = strtok(NULL, " ");			// 2 argumetos
+       if(delim != NULL){
+    	    strcpy(argumento2, delim);
         }
-    }else {
-        strcpy(orden, delim);
-    }
+  	}
 
     return 0;
 }
-
-
 
 void Procs_salir() {
 	exit(-1);
@@ -228,8 +248,8 @@ int main() {
 	memcpy(&ext_bytemaps, (EXT_BLQ_INODOS*)&datosfich[1], SIZE_BLOQUE);
 	memcpy(&ext_blq_inodos, (EXT_BLQ_INODOS*)&datosfich[2], SIZE_BLOQUE);
 	memcpy(&memdatos, (EXT_DATOS*)&datosfich[4], MAX_BLOQUES_DATOS * SIZE_BLOQUE);
-
-  	for(;;) {
+	
+  	for (;;) {
 		do {
 			printf(">> ");
 			fflush(stdin);
@@ -258,28 +278,20 @@ int main() {
 			else if(strcmp(orden,"remove")==0){
 				Borrar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, argumento1,  fent);
 				continue;
-			}
+			}	 
 			else if(strcmp(orden,"copy")==0){
 				Copiar(directorio, &ext_blq_inodos, &ext_bytemaps, &ext_superblock, memdatos, argumento1, argumento2,  fent);
 				continue;
 			}
-		/*	// Escritura de metadatos en comandos rename, remove, copy
-			Grabarinodosydirectorio(&directorio, &ext_blq_inodos, fent);			// check
-			GrabarByteMaps(&ext_bytemaps, fent);
-			GrabarSuperBloque(&ext_superblock, fent);
-			if (grabardatos)
-				GrabarDatos(&memdatos, fent);
-			grabardatos = 0;
-			//Si el comando es salir se habrÃ¡n escrito todos los metadatos
-			//faltan los datos y cerrar */
 	        else if (strcmp(orden,"salir")==0){
 	            //GrabarDatos(&memdatos,fent);
 	            fclose(fent);
 	       	    return 0;
-	        }
+	        }													// grabar datos despues de metodos modificadores
 			else{
 				printf("ERROR: Comando ilegal [info, bytemaps, dir, rename, imprimir, remove, copy, salir]\n");
 				continue;
-			}
+			} 
 	}
 }
+ 
